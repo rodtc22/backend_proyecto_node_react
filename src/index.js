64af5b5161eedda90a2c6feca712c6db30a8dba4
rodtc22 <rodrigoticonacoronel.22@gmail.com>
2023-06-07 08:15:49 -0400

    Agregando cors, para conectar desde otros servidores

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 // const express = require('express')
 import express from "express";
-require('dotenv').config()
-
+import cors from "cors";
 import rutasAuth from "./routes/auth.routes";
 
+
+require('dotenv').config()
+
 const app = express();
+
+// habilitamos CORS
+app.use(cors());
 
 // para capturar datos del req.body
 app.use(express.json()); 
