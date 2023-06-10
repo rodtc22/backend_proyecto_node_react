d1a264b202064ee3e19200970ca229201634d089
rodtc22 <rodrigoticonacoronel.22@gmail.com>
2023-06-10 14:56:40 -0400

    agregando buscador a categoriacontroller.listar y probando con categoria.rest

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -9,7 +9,7 @@ const Route = Router();
 //rutas categoria
 Route.get('/categoria', categoriaController.listar);
 Route.post('/categoria', categoriaController.guardar);
-Route.get('/categoria:/id', categoriaController.mostrar);
+Route.get('/categoria/:id', categoriaController.mostrar);
 Route.put('/categoria/:id', categoriaController.modificar);
 Route.delete('/categoria/:id', categoriaController.eliminar);
 
