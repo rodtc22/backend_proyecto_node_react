8946b28fd6cf7d3e9ba9cabe03c1277959f1eb0b
rodtc22 <rodrigoticonacoronel.22@gmail.com>
2023-06-22 22:15:18 -0400

    agregamos productoController, agregamos rutasProducto

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,4 +1,5 @@
 import categoriaController from "../controllers/categoria.controller";
+import productoController from "../controllers/producto.controller";
 import auhtMiddleware from "../middlewares/auth.middleware";
 const { Router } = require("express");
 
@@ -12,5 +13,12 @@ Route.post('/categoria', auhtMiddleware, categoriaController.guardar);
 Route.get('/categoria/:id', auhtMiddleware, categoriaController.mostrar);
 Route.put('/categoria/:id', auhtMiddleware, categoriaController.modificar);
 Route.delete('/categoria/:id', auhtMiddleware, categoriaController.eliminar);
+
+//rutas producto
+Route.get('/producto', auhtMiddleware, productoController.listar);
+Route.post('/producto', auhtMiddleware, productoController.guardar);
+Route.get('/producto/:id', auhtMiddleware, productoController.mostrar);
+Route.put('/producto/:id', auhtMiddleware, productoController.modificar);
+Route.delete('/producto/:id', auhtMiddleware, productoController.eliminar);
 
 export default Route;
